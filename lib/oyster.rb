f9426ce5a57d0e1600aec274f37d73b65b106c8c
EC2 Default User <ec2-user@ip-172-31-38-185.ec2.internal>
2020-07-13 15:28:59 +0000

    top up functionality added

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -6,4 +6,9 @@ class Oystercard
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
+  
+  def top_up(amount)
+    raise "Balance at capacity" if @balance >= 90
+    @balance += amount
+  end
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -5,5 +5,14 @@ describe Oystercard do
     it "starts at 0" do
       expect(subject.balance).to eq 0
     end
+    
+    it "is able to be topped up" do
+      expect(subject.top_up(5)).to eq 5
+    end
+    
+    it "has a maximum balance of 90" do
+      oyster = Oystercard.new(90)
+      expect{ oyster.top_up(1) }.to raise_error "Balance at capacity"
+    end
   end
 end
