505987461657e834d1f0274e252916e5833f7e62
EC2 Default User <ec2-user@ip-172-31-63-76.ec2.internal>
2020-07-14 15:00:31 +0000

    added journey functionality

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,14 +1,32 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :status
   
   DEFAULT_BALANCE = 0
+  MAXIMUM_BALANCE = 90
   
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
   
   def top_up(amount)
-    raise "Balance at capacity" if @balance >= 90
+    raise "Balance at capacity: #{MAXIMUM_BALANCE}" if @balance >= MAXIMUM_BALANCE
     @balance += amount
+  end
+  
+  def deduct(amount)
+    @balance -= amount
+  end
+  
+  def touch_in
+    @status = 1
+  end
+  
+  def touch_out
+    @status = 0
+  end
+  
+  def in_journey?
+    return true if @status == 1
+    return false if @status == 0
   end
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -12,7 +12,29 @@ describe Oystercard do
     
     it "has a maximum balance of 90" do
       oyster = Oystercard.new(90)
-      expect{ oyster.top_up(1) }.to raise_error "Balance at capacity"
+      expect{ oyster.top_up(1) }.to raise_error "Balance at capacity: 90"
     end
+    
+    it "can have money deducted from it" do
+      subject.top_up(50)
+      subject.deduct(40)
+      expect(subject.balance).to eq 10
+    end
+  end
+  
+  it "registers the beginning of a journey" do
+    subject.touch_in
+    expect(subject.status).to eq 1
+  end
+  
+  it "registers the end of a journey" do
+    subject.touch_in
+    subject.touch_out
+    expect(subject.status).to eq 0
+  end
+  
+  it "determines whether or not we are currently on a journey" do
+    subject.touch_in
+    expect(subject.in_journey?).to eq true
   end
 end
